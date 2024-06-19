d47dc6e33f731753756fbdd2d56092245ad1ae39
Firman Maulana <147988428+ChipiChapaa@users.noreply.github.com>
2024-06-19 14:28:11 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","db_dpw1");
+$koneksi = mysqli_connect("db","muriacell","muriacell","muriacell");
 
 if(mysqli_connect_errno()){
     echo "Koneksi database gagal: " . mysqli_connect_error();
